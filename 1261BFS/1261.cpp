624fe9229a0051ed324247373bf1c848689be800
NiklasJang <niklasjang@gmail.com>
2019-05-15 01:31:05 +0900

    1261BFS

---
+++ 1261BFS/1261.cpp
@@ -0,0 +1,66 @@
+/*
+2019-00-00
+장환석
+
+*/
+
+#include <iostream>
+#include <queue>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int N, M;
+int a[101][101];
+int check[101][101]; //얼마나 많은 벽을 부수고 왔는지를 카운트, 방문하지 않았으면 -1
+int dh[4] = { 0, 0, 1, -1 };
+int dw[4] = { 1, -1, 0, 0 };
+int main(void) {
+	scanf_s("%d %d", &M, &N);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++){
+			scanf_s("%1d", &a[i][j]);
+		}
+	}
+
+	memset(check, -1, sizeof(check));
+
+	queue<pair<int, int> > q;
+
+	check[0][0]= 0; //방문, 0개 부수고 옴
+	q.push(make_pair(0, 0)); 
+	int index = 0;
+	while (!q.empty()) {
+		pair<int, int> curr = q.front(); q.pop();
+		int h = curr.first; int w = curr.second;
+		for (int k = 0; k < 4; k++) {
+			int nh = h + dh[k]; //next h
+			int nw = w + dw[k]; //next w
+			if (nh <0 || nh >= N) continue;
+			if (nw <0 || nw >= M) continue;
+			if (check[nh][nw] == -1 ) {
+				//다음 방문할 곳이 벽이면 1 추가 아니면 그대로
+				check[nh][nw] = check[h][w] + a[nh][nw]; 
+				q.push(make_pair(nh, nw));
+			}else{
+				//이미 방문한 곳이면 기존의 값이랑 현재에서 가는 값이랑 비교에서 작은 걸로 저장
+				if (check[nh][nw] > check[h][w] + a[nh][nw]) {
+					check[nh][nw] = check[h][w] + a[nh][nw];
+					q.push(make_pair(nh, nw)); //기존에 방문했던 점이라도 현재의 점을 지나서 방문하면 q에 넣어줘야 한다.
+				}
+			}
+		}
+	}
+	/*
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%d ", check[i][j]);
+		}
+		puts("");
+	}*/
+
+	printf("%d ", check[N - 1][M - 1]);
+	
+	
+	return 0;
+}
